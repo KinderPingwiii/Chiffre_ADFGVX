181f25851f0117d1f6b747b9873e04b9619c6dbc
Picpic <maxime.picard.oise@gmail.com>
2020-02-05 12:13:39 +0100

    Création tableau

--- Chiffre_ADFGVX.cpp
+++ Chiffre_ADFGVX.cpp
@@ -1,7 +1,18 @@
-#include "pch.h"
+/*****************************************************************
+Objectif .... : Chiffrer et déchiffrer des phrases en se basant
+				sur la méthode de chiffrement ADFGVX
+Auteurs ..... : LHEUREUX Jordan
+				BAUDELET Pierre
+				PICARD Maxime
+Version ..... : V1.0 sur 05/02/2020
+*****************************************************************/
+
+#include "Crypt_chiffre_ADFGVX.h"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+using namespace std;
+
+int main(){
+	char table[6][6];
+	Create_Table(table);
 }
--- Crypt_chiffre_ADFGVX.h
+++ Crypt_chiffre_ADFGVX.h
@@ -1,12 +1,90 @@
 #pragma once
+
+#include <iostream>
+using namespace std;
+
 class Crypt_chiffre_ADFGVX
 {
 public:
 	Crypt_chiffre_ADFGVX();
 	void Encryption();
 	void Decryption();
-	~Crypt_chiffre_ADFGVX();
+	~Crypt_chiffre_ADFGVX();//Déclaration du tableau
 
 
 };
 
+void Create_Table(char tableau[6][6]) //Déclaration du tableau
+{
+    int i,j;
+	/*Initialisation des caractères*/
+	tableau[0][0]=' ';
+	tableau[0][1]='#';
+	tableau[0][2]='+';
+	tableau[0][3]='%';
+	tableau[0][4]='-';
+	tableau[0][5]='.';
+	tableau[0][6]='*';
+	tableau[1][0]='#';
+	tableau[2][0]='+';
+	tableau[3][0]='%';
+	tableau[4][0]='-';
+	tableau[5][0]='.';
+	tableau[6][0]='*';
+
+	/*Initialisation des caractères une fois déchiffrés*/
+
+	//Colonne 1
+	tableau[1][1]='0';
+	tableau[2][1]='1';
+	tableau[3][1]='2';
+	tableau[4][1]='3';
+	tableau[5][1]='4';
+	tableau[6][1]='5';
+
+	//Colonne 2
+	tableau[1][2]='a';
+	tableau[2][2]='b';
+	tableau[3][2]='c';
+	tableau[4][2]='d';
+	tableau[5][2]='e';
+	tableau[6][2]='f';
+
+	//Colonne 3
+	tableau[1][3]='i';
+	tableau[2][3]='j';
+	tableau[3][3]='k';
+	tableau[4][3]='l';
+	tableau[5][3]='m';
+	tableau[6][3]='n';
+
+	//Colonne 4
+	tableau[1][4]='u';
+	tableau[2][4]='v';
+	tableau[3][4]='w';
+	tableau[4][4]='x';
+	tableau[5][4]='y';
+	tableau[6][4]='z';
+
+	//Colonne 5
+	tableau[1][5]='o';
+	tableau[2][5]='p';
+	tableau[3][5]='q';
+	tableau[4][5]='r';
+	tableau[5][5]='s';
+	tableau[6][5]='t';
+
+	//Colonne 6
+	tableau[1][6]='6';
+	tableau[2][6]='7';
+	tableau[3][6]='8';
+	tableau[4][6]='9';
+	tableau[5][6]='g';
+	tableau[6][6]='h';
+
+	for (i=0;i<=6;i++){
+		for (j=0;j<=6;j++){
+			cout<<tableau[j][i]<<endl;
+		}
+	}
+}
