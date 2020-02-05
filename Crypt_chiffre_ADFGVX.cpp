cdaa6c22dddfb3b9be802116380278ab5944a53b
Deykid <33992397+deykid@users.noreply.github.com>
2020-02-05 10:02:31 +0100

    Add files via upload

---
+++ Crypt_chiffre_ADFGVX.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "Crypt_chiffre_ADFGVX.h"
+
+Crypt_chiffre_ADFGVX::Crypt_chiffre_ADFGVX()
+{
+
+}
+
+void Crypt_chiffre_ADFGVX::Encryption()
+{
+
+}
+
+void Crypt_chiffre_ADFGVX::Decryption()
+{
+
+}
+
+
+Crypt_chiffre_ADFGVX::~Crypt_chiffre_ADFGVX()
+{
+
+}
---
+++ Crypt_chiffre_ADFGVX.h
@@ -0,0 +1,12 @@
+#pragma once
+class Crypt_chiffre_ADFGVX
+{
+public:
+	Crypt_chiffre_ADFGVX();
+	void Encryption();
+	void Decryption();
+	~Crypt_chiffre_ADFGVX();
+
+
+};
+
